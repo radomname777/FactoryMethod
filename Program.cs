04560c753ba217080abcbbd69798f5e16c21b7fc
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-07-12 22:25:38 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using FactoryMethod.Abstract;
+using FactoryMethod.Concrete;
+
+namespace FactoryMethod;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        Creator? logistics = null;
+        Product? transport = null;
+
+        while (true)
+        {
+            Console.WriteLine(@"
+                                1: ProductA
+                                2: ProductB
+                                Any: Exit");
+
+            Console.Write("\n\tEnter choice: ");
+
+
+            logistics = Console.ReadLine() switch
+            {
+                "1" => new ConcreteCreatorA(),
+                "2" => new ConcreteCreatorB(),
+                _ => null
+            };
+
+            if (logistics == null)
+                break;
+            transport = logistics?.CreateProduct();
+            transport?.DoStuff();
+        }
+    }
+}
